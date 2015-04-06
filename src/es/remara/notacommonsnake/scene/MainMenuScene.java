8175d29291aa73955aa4f828cca04bc6a7e76083
JavideM <javierdemartinrivares@gmail.com>
2015-04-06 17:46:49 +0200

    Añadida escena Menu

---
+++ src/es/remara/notacommonsnake/scene/MainMenuScene.java
@@ -0,0 +1,43 @@
+package es.remara.notacommonsnake.scene;
+
+import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.andengine.entity.scene.menu.item.IMenuItem;
+
+import es.remara.notacommonsnake.base.BaseScene;
+import es.remara.notacommonsnake.manager.SceneManager.SceneType;
+
+public class MainMenuScene extends BaseScene implements IOnMenuItemClickListener{
+
+	@Override
+	public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem,
+			float pMenuItemLocalX, float pMenuItemLocalY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onBackKeyPressed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SceneType getSceneType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void disposeScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
