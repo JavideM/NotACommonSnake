3f1c9cf1361bf3242a33919d0da2184c0f2e1b7f
Rithlock <ivanreyerossantos@gmail.com>
2015-04-24 23:10:42 +0200

    Comida normal y añadidas imagenes al resourceManager.
    Comida normal y añadidas imagenes al resourceManager.

--- src/es/remara/notacommonsnake/manager/ResourcesManager.java
+++ src/es/remara/notacommonsnake/manager/ResourcesManager.java
@@ -30,19 +30,35 @@ public class ResourcesManager
 // TEXTURES & TEXTURE REGIONS
 //---------------------------------------------
 	
-	// Menus and Splash Textures
+	// Texture Regions
 	public ITextureRegion splash_region;
 	public ITextureRegion menu_background_region;
 	public ITextureRegion play_region;
 	public ITextureRegion options_region;
 	public ITextureRegion wip_region; 
 	public ITextureRegion ark_ball_region;
+	public ITextureRegion background_grass_region;
+	public ITextureRegion food_X2_region;
+	public ITextureRegion food_AUG_SPEED_region;
+	public ITextureRegion food_REDUC_SPEED_region;
+	public ITextureRegion food_CHG_GAME_MODE_region;
+	public ITextureRegion food_GHOST_MODE_region;
+	public ITextureRegion food_SUPER_GROW_region;
+	public ITextureRegion food_INV_CONTROLS_region;
+	public ITextureRegion food_NORMAL_region;
+	public ITextureRegion snake_body_region;
+	public ITextureRegion snake_head_region;
+	public ITextureRegion snake_tail_region;
+	public ITextureRegion snake_corner_region;
+	public ITextureRegion wall_region;
+	
 	
 	// Bipmat Textures
 	private BitmapTextureAtlas splashTextureAtlas;
 	private BuildableBitmapTextureAtlas menuTextureAtlas;
 	private BitmapTextureAtlas wipTextureAtlas;
 	private BitmapTextureAtlas ark_ballTextureAtlas;
+	private BitmapTextureAtlas snakeTextureAtlas;
 
 	
 //---------------------------------------------
@@ -58,14 +74,14 @@ public void loadMenuResources()
     
     public void loadGameResources()
     {
-        loadGameGraphics();
-        loadGameFonts();
-        loadGameAudio();
+        loadGameSnakeGraphics();
+        loadGameSnakeFonts();
+        loadGameSnakeAudio();
     }
     
     public void loadGameArkanoidResources() {
 		loadGameArkanoidGraphics();
-		loadGameFonts();
+		loadGameSnakeFonts();
 		loadGameArkanoidAudio();
 	}
     
@@ -115,17 +131,33 @@ public void loadMenuResources()
     	
     }
 
-    private void loadGameGraphics()
+    private void loadGameSnakeGraphics()
     {
         
     }
     
-    private void loadGameFonts()
+    private void loadGameSnakeFonts()
     {
-        
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+		snakeTextureAtlas = new BitmapTextureAtlas(activity.getTextureManager(), 1024, 1024, TextureOptions.BILINEAR);
+		background_grass_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "backgraound/grass.png", 0, 0);
+		food_X2_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_aqua.png", 0, 0);
+		food_AUG_SPEED_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_black.png", 0, 0);
+		food_REDUC_SPEED_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_blue.png", 0, 0);
+		food_CHG_GAME_MODE_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_green.png", 0, 0);
+		food_GHOST_MODE_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_pink.png", 0, 0);
+		food_SUPER_GROW_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_red.png", 0, 0);
+		food_INV_CONTROLS_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_yellow.png", 0, 0);
+		food_NORMAL_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/food_lsd.png", 0, 0);
+		snake_body_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/snake_body.png", 0, 0);
+		snake_head_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/snake_head.png", 0, 0);
+		snake_tail_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/snake_tail.png", 0, 0);
+		snake_corner_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/snake_corner.png", 0, 0);
+		wall_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(snakeTextureAtlas, activity, "snake/wall.png", 0, 0);
+		snakeTextureAtlas.load();
     }
     
-    private void loadGameAudio()
+    private void loadGameSnakeAudio()
     {
         
     }
