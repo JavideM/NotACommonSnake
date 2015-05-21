5b669fadf67eed18b38e1fcbde6d043de39e0c8a
JavideM <javierdemartinrivares@gmail.com>
2015-05-21 15:51:32 +0200

    Update Profile.java

--- src/es/remara/notacommonsnake/model/Profile.java
+++ src/es/remara/notacommonsnake/model/Profile.java
@@ -1,5 +1,41 @@
 package es.remara.notacommonsnake.model;
 
 public class Profile{
-
+  /*
+  * Parameters
+  */
+  private int idprofile;
+  private String name;
+  private boolean active;
+  private ArrayList<Session> sessions;
+  
+  /*
+  * Getters Setters
+  */
+  public int getIdprofile(){
+    return this.idprofile;
+  }
+  public void setIdprofile(int idprofile){
+    this.idprofile = idprofile;
+  }
+  public String getName(){
+    return this.name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+  public boolean isActive(){
+    return this.active;
+  }
+  public void setActive(boolean active){
+    this.active = active;
+  }
+  /*
+  * Contructor
+  */
+  public Profile(){
+    //
+    // Contructor Logic Here
+    //
+  }
 }
