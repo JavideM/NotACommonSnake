6bf4457797b62b959fbcbffaa9a823115ec0d84c
JavideM <javierdemartinrivares@gmail.com>
2015-06-07 21:02:13 +0200

    Achivementsandmorestats
    ARS finished

--- src/es/remara/notacommonsnake/scene/AchievementsRecordsStatsScene.java
+++ src/es/remara/notacommonsnake/scene/AchievementsRecordsStatsScene.java
@@ -141,8 +141,36 @@ public class AchievementsRecordsStatsScene extends BaseScene {
 		 */
 		Sprite pannel = new Sprite(camera.getWidth() / 2,
 				camera.getHeight() / 2 + 30,
-				resourcesManager.orange_pannel_region, vbom);
+				resourcesManager.orange_pannel_region, vbom){
+			float positionIn = 0;
+
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+					final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				// Scroll functionality
+				if(achievementsPannel.isVisible()){
+					if (pSceneTouchEvent.isActionDown())
+						positionIn = pTouchAreaLocalY;
+					float distance = (positionIn - pTouchAreaLocalY) / 10;
+					if ((achievementslist.get(0).getY() - distance) >= top_ini_position - 5
+							&& (achievementslist.get(achievementslist.size() - 1).getY() - distance <= bottom_ini_position + 5)) {
+						for (int i = 0; i < achievementslist.size(); i++) {
+							achievementslist.get(i).setPosition(achievementslist.get(i).getX(),
+									achievementslist.get(i).getY() - distance);
+							if (achievementslist.get(i).getY() >= top_ini_position + 5
+									|| achievementslist.get(i).getY() <= bottom_ini_position - 5) {
+								achievementslist.get(i).setVisible(false);
+							} else {
+								achievementslist.get(i).setVisible(true);
+							}
+						}
+					}
+				}
+				return true;
+			}
+		};
 		achievementsPannel.attachChild(pannel);
+		registerTouchArea(pannel);
 		attachChild(achievementsPannel);
 		achievementsPannel.setVisible(false);
 
@@ -157,38 +185,38 @@ public class AchievementsRecordsStatsScene extends BaseScene {
 		achievementsPannel.attachChild(text);
 		//Main Content
 		//Achievements
-				int count = 1;
-				achievementslist = new ArrayList<Text>();
-				ArrayList<Achievement> achievements = (ArrayList<Achievement>) dbmanager
-						.getAllAchievementsDone();
-				for (Achievement achievement : achievements) {
-					String content = count + ". " + achievement.getName();
-					// Name and position
-					Text textName = new Text(camera.getHeight() / 2,
-							camera.getHeight()
-									- (camera.getHeight() / 12 + camera.getHeight()
-											/ 12 * count), resourcesManager.fontARS,
-							content, new TextOptions(HorizontalAlign.LEFT), this.vbom);
-					textName.setPosition(textName.getX() + textName.getWidth()
-							/ 2, textName.getY());
-					// Description
-					content = achievement.getDescription();
-					Text textDescript = new Text(600, camera.getHeight()
-							- (camera.getHeight() / 12 + camera.getHeight() / 12
-									* count), resourcesManager.fontARS, content,
-							new TextOptions(HorizontalAlign.LEFT), this.vbom);
-					textDescript.setPosition(textDescript.getX() - textDescript.getWidth() / 2,
-							textDescript.getY());
-					achievementsPannel.attachChild(textName);
-					achievementsPannel.attachChild(textDescript);
-					achievementslist.add(textName);
-					achievementslist.add(textDescript);
-					if (count > 8) {
-						textName.setVisible(false);
-						textDescript.setVisible(false);
-					}
-					count++;
-				}
+		int count = 1;
+		achievementslist = new ArrayList<Text>();
+		ArrayList<Achievement> achievements = (ArrayList<Achievement>) dbmanager
+				.getAllAchievementsDone();
+		for (Achievement achievement : achievements) {
+			String content = count + ". " + achievement.getName();
+			// Name and position
+			Text textName = new Text(camera.getWidth() / 5,
+					camera.getHeight()
+							- (camera.getHeight() / 12 + camera.getHeight()
+									/ 12 * count), resourcesManager.fontARS,
+					content, new TextOptions(HorizontalAlign.LEFT), this.vbom);
+			textName.setPosition(textName.getX() + textName.getWidth()
+					/ 2, textName.getY());
+			// Description
+			content = achievement.getDescription();
+			Text textDescript = new Text(camera.getWidth()/2, camera.getHeight()
+					- (camera.getHeight() / 12 + camera.getHeight() / 12
+							* count), resourcesManager.fontARS, content,
+					new TextOptions(HorizontalAlign.LEFT), this.vbom);
+			textDescript.setPosition(textDescript.getX() + textDescript.getWidth() / 2,
+					textDescript.getY());
+			achievementsPannel.attachChild(textName);
+			achievementsPannel.attachChild(textDescript);
+			achievementslist.add(textName);
+			achievementslist.add(textDescript);
+			if (count > 8) {
+				textName.setVisible(false);
+				textDescript.setVisible(false);
+			}
+			count++;
+		}
 	}
 
 	private void createRecordsPannel() {
@@ -244,7 +272,7 @@ public class AchievementsRecordsStatsScene extends BaseScene {
 		for (Session session : sessions) {
 			String content = count + ". " + session.getPlayer_name();
 			// Name and position
-			Text textPlayer = new Text(camera.getHeight() / 2,
+			Text textPlayer = new Text(camera.getWidth() / 4,
 					camera.getHeight()
 							- (camera.getHeight() / 12 + camera.getHeight()
 									/ 12 * count), resourcesManager.fontARS,
@@ -296,62 +324,72 @@ public class AchievementsRecordsStatsScene extends BaseScene {
 						HorizontalAlign.CENTER), this.vbom);
 		statisticPannel.attachChild(text);
 		//Main Content
+		//Total score
 		Text titleTotal = new Text(camera.getWidth() / 4,
 				camera.getHeight()
-				- (camera.getHeight() / 12 + camera.getHeight()
-						/ 12 ), resourcesManager.fontARS,
+				- (camera.getHeight() / 12 * 2), resourcesManager.fontARS,
 						activity.getString(R.string.totalscore), new TextOptions(HorizontalAlign.LEFT), this.vbom);
 		titleTotal.setPosition(titleTotal.getX() + titleTotal.getWidth()/2, titleTotal.getY());
 		statisticPannel.attachChild(titleTotal);
-		Text textTotal = new Text(2*camera.getWidth() / 3,
+		Text textTotal = new Text(3*camera.getWidth() / 4,
 				camera.getHeight()
-						- (camera.getHeight() / 12 + camera.getHeight()
-								/ 12 ), resourcesManager.fontARS,
+						- (camera.getHeight() / 12 * 2 ), resourcesManager.fontARS,
 				""+totalScore(), new TextOptions(HorizontalAlign.RIGHT), this.vbom);
 		statisticPannel.attachChild(textTotal);
 		textTotal.setPosition(textTotal.getX() - textTotal.getWidth()/2, textTotal.getY());
 		
+		//Higher level
 		Text titleHigher = new Text(camera.getWidth() / 4,
 				camera.getHeight()
-				- (camera.getHeight() / 12 + camera.getHeight()
-						/ 12 * 3), resourcesManager.fontARS,
+				- (camera.getHeight() / 12 *8 - 3*camera.getHeight()/ 8), resourcesManager.fontARS,
 						activity.getString(R.string.higherlevel), new TextOptions(HorizontalAlign.LEFT), this.vbom);
 		titleHigher.setPosition(titleHigher.getX() + titleHigher.getWidth()/2, titleHigher.getY());
 		statisticPannel.attachChild(titleHigher);
-		Text textHigherLevel = new Text(2*camera.getWidth() / 3,
+		Text textHigherLevel = new Text(3*camera.getWidth() / 4,
 				camera.getHeight()
-						- (camera.getHeight() / 12 + camera.getHeight()
-								/ 12 * 3), resourcesManager.fontARS,
+						- (camera.getHeight() / 12 *8 - 3*camera.getHeight()/ 8), resourcesManager.fontARS,
 				""+higherLevel(), new TextOptions(HorizontalAlign.RIGHT), this.vbom);
 		statisticPannel.attachChild(textHigherLevel);
 		textHigherLevel.setPosition(textHigherLevel.getX() - textHigherLevel.getWidth()/2, textHigherLevel.getY());
 		
+		//Sessions played
+		Text titleSessions = new Text(camera.getWidth() / 4,
+				camera.getHeight()
+				- (camera.getHeight() / 12 *8 - camera.getHeight()/ 4), resourcesManager.fontARS,
+						activity.getString(R.string.sessionsplayed), new TextOptions(HorizontalAlign.LEFT), this.vbom);
+		titleSessions.setPosition(titleSessions.getX() + titleSessions.getWidth()/2, titleSessions.getY());
+		statisticPannel.attachChild(titleSessions);
+		Text textSessions = new Text(3*camera.getWidth() / 4,
+				camera.getHeight()
+						- (camera.getHeight() / 12 *8 - camera.getHeight()/ 4), resourcesManager.fontARS,
+				""+sessions.size(), new TextOptions(HorizontalAlign.RIGHT), this.vbom);
+		statisticPannel.attachChild(textSessions);
+		textSessions.setPosition(textSessions.getX() - textSessions.getWidth()/2, textSessions.getY());
+		
+		//Average score
 		Text titleAverage = new Text(camera.getWidth() / 4,
 				camera.getHeight()
-				- (camera.getHeight() / 12 + camera.getHeight()
-						/ 12 * 5), resourcesManager.fontARS,
+				- (camera.getHeight() / 12 *8 - camera.getHeight()/ 8), resourcesManager.fontARS,
 						activity.getString(R.string.avergaescore), new TextOptions(HorizontalAlign.LEFT), this.vbom);
 		titleAverage.setPosition(titleAverage.getX() + titleAverage.getWidth()/2, titleAverage.getY());
 		statisticPannel.attachChild(titleAverage);
-		Text textaverage = new Text(2*camera.getWidth() / 3,
+		Text textaverage = new Text(3*camera.getWidth() / 4,
 				camera.getHeight()
-						- (camera.getHeight() / 12 + camera.getHeight()
-								/ 12 * 5), resourcesManager.fontARS,
+						- (camera.getHeight() / 12 *8 - camera.getHeight()/ 8), resourcesManager.fontARS,
 				""+averageScore(), new TextOptions(HorizontalAlign.RIGHT), this.vbom);
 		statisticPannel.attachChild(textaverage);
 		textaverage.setPosition(textaverage.getX() - textaverage.getWidth()/2, textaverage.getY());
 		
+		//Average per level
 		Text titlePerLevel = new Text(camera.getWidth() / 4,
 				camera.getHeight()
-				- (camera.getHeight() / 12 + camera.getHeight()
-						/ 12 * 7), resourcesManager.fontARS,
+				- (camera.getHeight() / 12 *8), resourcesManager.fontARS,
 						activity.getString(R.string.scoreperlevel), new TextOptions(HorizontalAlign.LEFT), this.vbom);
 		titlePerLevel.setPosition(titlePerLevel.getX() + titlePerLevel.getWidth()/2, titlePerLevel.getY());
 		statisticPannel.attachChild(titlePerLevel);
-		Text textaverageScorePerLevel = new Text(2*camera.getWidth() / 3,
+		Text textaverageScorePerLevel = new Text(3*camera.getWidth() / 4,
 				camera.getHeight()
-						- (camera.getHeight() / 12 + camera.getHeight()
-								/ 12 * 7), resourcesManager.fontARS,
+						- (camera.getHeight() / 12 * 8), resourcesManager.fontARS,
 				""+averageScorePerLevel(), new TextOptions(HorizontalAlign.RIGHT), this.vbom);
 		statisticPannel.attachChild(textaverageScorePerLevel);
 		textaverageScorePerLevel.setPosition(textaverageScorePerLevel.getX() - textaverageScorePerLevel.getWidth()/2, textaverageScorePerLevel.getY());
@@ -379,7 +417,7 @@ public class AchievementsRecordsStatsScene extends BaseScene {
 		int total = totalScore();
 		int totallevels = 0;
 		for (Session session : sessions) {
-			totallevels += session.getLevel();
+			totallevels += session.getLevel() + 1;
 		}
 		return (totallevels == 0)? 0: total/totallevels;
 	}
